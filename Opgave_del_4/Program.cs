783b0e0e1467f9945f69591227c4992e97f4b8fe
Sanj <Sanj@DESKTOP-E5RNT0U>
2022-11-25 02:13:15 +0100

    25/11/2022 02:13 WIP

--- Opgave_del_4/Program.cs
+++ Opgave_del_4/Program.cs
@@ -73,143 +73,128 @@ foreach (var item in _enrollmentList2)
 }
 */
 //-----------------------------------------------------------------------
-Enrollment _enrollmentList = new();
+//Enrollment _enrollmentList = new();
 
-_enrollmentList.EnrollList = new();
+//_enrollmentList.EnrollList = new();
+List<Enrollment> _enrollmentList = new List<Enrollment>();
 
 
-bool menuLoop = true;
+bool menuLoop = false;
 
 string? firstName;
 string? lastName;
 string? dateOfBirth;
 string? course;
+string? addCourses;
+bool moreCourses = false;
+string? addStudent;
+bool success = false;
+int studentID;
 
 /*
  ------------------------------------------------------------------------------
                             Menu
  ------------------------------------------------------------------------------
  */
-while (menuLoop)
+do
 {
-    Console.WriteLine("1) Add a student to a Course:");
+    Console.WriteLine("1) Add a student to a Course");
+    Console.Write("\nChoose 1");
 
     string? userInput = Console.ReadLine();
 
 
-    if (userInput == "1")
+    if (userInput != "1")
     {
+        Console.WriteLine("Please choose one of the Options.");
+        Console.ReadKey();
+        Console.Clear();
+        break;
+    }
+    do
+    {
+
         Console.Clear();
         Console.Write("Give Student ID: ");
-        bool success = int.TryParse(Console.ReadLine(), out int studentID);
+        success = int.TryParse(Console.ReadLine(), out studentID);
         if (!success)
         {
             Console.WriteLine("Error: Write a number not a letter.");
-        }
-        while (success)
-        {
-
-
-            Console.Write("Student First Name: ");
-            firstName = Console.ReadLine();
-
-            Console.Write("Student Last Name: ");
-            lastName = Console.ReadLine();
-
-            Console.Write("Student Date of Birth (dd-mm-yyyy): ");
-            //dateOfBirth.
-            dateOfBirth = Console.ReadLine();
-
-            Console.WriteLine("Choose Course: \n----------------------------------------------------");
-            foreach (CourseEnum courseEnum in Enum.GetValues(typeof(CourseEnum)))
-            {
-                string courseEnums = courseEnum.ToString();
-                if (courseEnums != "Unkown")
-                {
-                    Console.WriteLine(courseEnum);
-                }
-            }
-
-            Students student = new(studentID, firstName, lastName, dateOfBirth);
-          
-
-            Console.WriteLine("----------------------------------------------------");
-            course = Console.ReadLine().ToUpper();
-            CourseEnum _courseEnum;
-            //check if input course is in enum list, ignoring case
-            if (Enum.TryParse<CourseEnum>(course, true, out _courseEnum))
-            {
-                //Console.WriteLine("{0} converted to {1}", course, _courseEnum);
-                int courseIndex = courseEnumDict.GetValueOrDefault(_courseEnum);
-                Course courses = courseList[courseIndex];
-                List<Enrollment> _enrollmentList2 = new List<Enrollment>();
-                _enrollmentList2.Add(new Enrollment(student, courses));
-
-                foreach (var item in _enrollmentList2)
-                {
-                    Console.WriteLine($"\n{item.StudentInfo.FirstName} {item.StudentInfo.LastName}  {item.StudentInfo.Age} Course: {item.CoursesInfo.CourseName}  Teacher: {item.CoursesInfo.Teachers.FirstName} {item.CoursesInfo.Teachers.LastName} \n");
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("Error: Course Not found. Try again");
-            }
-
-            //Outdated
-            /*
-            
-            if(CourseEnum.Clientsideprogrammering.ToString().ToUpper() == course)
-            {
-                _enrollmentList.EnrollList.Add(new Enrollment(student, ClientSide));
-            }
-
-            else if (CourseEnum.Studieteknik.ToString().ToUpper() == course)
-            {
-                _enrollmentList.EnrollList.Add(new Enrollment(student, StudieTeknik));
-            }
-
-            else if (CourseEnum.Grundlæggendeprogrammering.ToString() == course)
-            {
-                _enrollmentList.EnrollList.Add(new Enrollment(student, GrundlProg));
-            }
-
-            else if (CourseEnum.OOP.ToString() == course)
-            {
-                _enrollmentList.EnrollList.Add(new Enrollment(student, OOP));
-            }
-
-            else if (CourseEnum.Databaseprogrammering.ToString() == course)
-            {
-                _enrollmentList.EnrollList.Add(new Enrollment(student, Database));
-            }
-
-            else if (CourseEnum.Computerteknologi.ToString() == course)
-            {
-                _enrollmentList.EnrollList.Add(new Enrollment(student, Computerteknologi));
-            }
-
-            else if (CourseEnum.Netværk.ToString() == course)
-            {
-                _enrollmentList.EnrollList.Add(new Enrollment(student, Netværk));
-            }
-            
-            else
-            {
-                Console.WriteLine("Error: Course Not found. Try again");
-            }
-           
-
-            foreach (var item in _enrollmentList.EnrollList)
-            {
-                Console.WriteLine($"\n{item.StudentInfo.FirstName} {item.StudentInfo.LastName} Age: {item.StudentInfo.Age}  Course: {item.CoursesInfo.CourseName}  Teacher: {item.CoursesInfo.Teachers.FirstName} {item.CoursesInfo.Teachers.LastName}.\n");
-            }
-            */
+            Console.ReadKey();
         }
     }
+    while (!success);
 
-    else
+
+    Console.Write("Student First Name: ");
+    firstName = Console.ReadLine();
+
+    Console.Write("Student Last Name: ");
+    lastName = Console.ReadLine();
+
+    Console.Write("Student Date of Birth (dd-mm-yyyy): ");
+    //dateOfBirth.
+    dateOfBirth = Console.ReadLine();
+
+    Console.WriteLine("Choose Course: \n----------------------------------------------------");
+    foreach (CourseEnum courseEnum in Enum.GetValues(typeof(CourseEnum)))
     {
-        Console.WriteLine("Wrong Choice My lad");
+        string courseEnums = courseEnum.ToString();
+        if (courseEnums != "Unkown")
+        {
+            Console.WriteLine(courseEnum);
+        }
     }
+
+    Students student = new(studentID, firstName, lastName, dateOfBirth);
+
+    do
+    {
+
+        Console.WriteLine("----------------------------------------------------");
+        course = Console.ReadLine().ToUpper();
+        CourseEnum _courseEnum;
+        //check if input course is in enum list, ignoring case
+        if (Enum.TryParse<CourseEnum>(course, true, out _courseEnum))
+        {
+            //Console.WriteLine("{0} converted to {1}", course, _courseEnum);
+            int courseIndex = courseEnumDict.GetValueOrDefault(_courseEnum);
+            Course courses = courseList[courseIndex];
+            _enrollmentList.Add(new Enrollment(student, courses));
+
+            foreach (var item in _enrollmentList)
+            {
+                Console.WriteLine($"\n{item.StudentInfo.FirstName} {item.StudentInfo.LastName}  {item.StudentInfo.Age} Course: {item.CoursesInfo.CourseName}  Teacher: {item.CoursesInfo.Teachers.FirstName} {item.CoursesInfo.Teachers.LastName} \n");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Wrong Choice My lad");
+        }
+
+        Console.WriteLine("Press 1 to add more courses. Press any other key to exit");
+        addCourses = Console.ReadLine().ToUpper();
+        if (addCourses != "1")
+            moreCourses = false;
+    }
+    while (moreCourses);
+
+    Console.WriteLine("Press 1 to add Student or Any press any other keys to exit");
+    addStudent = Console.ReadLine();
+    Console.Clear();
+    if (addStudent != "1")
+    {
+        menuLoop = true;
+    }
+
+
 }
+while (!menuLoop);
+
+foreach (var item in _enrollmentList)
+{
+    Console.WriteLine($"{item.StudentInfo.FirstName} {item.StudentInfo.LastName} Age: {item.StudentInfo.Age}  Course: {item.CoursesInfo.CourseName}  Teacher: {item.CoursesInfo.Teachers.FirstName} {item.CoursesInfo.Teachers.LastName}.");
+}
+
+Console.WriteLine("Press enter to exit");
